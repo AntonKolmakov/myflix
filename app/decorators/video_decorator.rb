a07b2d7d509707d95518d354f5f611ba020aff18
AntonKolmakov <kolmakoff@icloud.com>
2014-06-19 00:31:58 +0600

    Write a sign up service

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -5,7 +5,7 @@ class VideoDecorator < Draper::Decorator
     object.rating.present? ? "#{object.rating}/5.0" : "N/A"
   end
 
-  def add_my_queue
+  def add_to_my_queue
     h.link_to "+ My Queue", h.queue_items_path(video_id: object.id), method: :post, class: 'btn btn-default' unless h.current_user.queued_video?(object)
   end
 end
