6ee7e77315d910bb5cfe2fa4ad4cee56fc253edd
AntonKolmakov <kolmakoff@icloud.com>
2014-06-22 00:51:50 +0600

    Admin views payments

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,6 +1,5 @@
-class Admin::VideosController < ApplicationController
+class Admin::VideosController < AdminsController
   before_filter :require_user
-  before_filter :require_admin
 
   def new
     @video = Video.new
@@ -21,12 +20,5 @@ class Admin::VideosController < ApplicationController
 
   def video_params
     params.require(:video).permit(:title, :description, :category_id, :small_cover, :large_cover, :video_url)
-  end
-
-  def require_admin
-    if !current_user.admin?
-      flash[:danger] = "You are not authorized to do that."
-      redirect_to home_path
-    end
   end
 end
