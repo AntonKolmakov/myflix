7ef4362be0170ecfb9bf185fe5704fdc976b4de8
AntonKolmakov <kolmakoff@icloud.com>
2014-06-25 21:41:19 +0600

    admin can see admin box

--- spec/features/admin_sees_payments_spec.rb
+++ spec/features/admin_sees_payments_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
-feature "Admin sees payments" do
+feature "Admin sees admin box" do
   background do
     alice = Fabricate(:user, full_name: "Alice Wonder", email: "alice@example.com")
     Fabricate(:payment, amount: 999, user: alice)
+  end
+
+  scenario "admin can see admin box" do
+    sign_in(Fabricate(:admin))
+    visit home_path
+    expect(page).to have_content("Admin")
+  end
+
+  scenario "user can't see admin box" do
+    sign_in(Fabricate(:user))
+    visit home_path
+    expect(page).not_to have_content("Admin")
   end
 
   scenario "admin can see payments" do
@@ -13,6 +25,7 @@ feature "Admin sees payments" do
     expect(page).to have_content("Alice Wonder")
     expect(page).to have_content("alice@example")
   end
+
   scenario "user cannot see payments" do
     sign_in(Fabricate(:user))
     visit admin_payments_path
